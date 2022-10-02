670823acb4d3a97786aca14336da4e47c0ebd64d
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-01 20:03:12 -0700

    Update functions.dart

--- examples/json/lib/functions.dart
+++ examples/json/lib/functions.dart
@@ -62,5 +62,4 @@ class GreetingResponse {
 GreetingResponse function(GreetingRequest request) {
   final name = request.name ?? 'World';
   final json = GreetingResponse(salutation: 'Hello', name: name);
-  return json;
-}
+ 
